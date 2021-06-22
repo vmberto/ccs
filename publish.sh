afefaf3fdb50322745894332281c1d12323dc0c0
UmbertoB <umbertobarrosf@gmail.com>
2021-06-22 20:49:59 -0300

    remove publish.sh

--- publish.sh
+++
@@ -1,3 +0,0 @@
-rm -rf dist/* && 
-python setup.py bdist_wheel && 
-python -m twine upload -u __token__ -p pypi-AgENdGVzdC5weXBpLm9yZwIkYTAyYzM5OTAtZjg1Yy00OGU5LWFiM2UtNjE0ZmIxY2VjZDllAAIleyJwZXJtaXNzaW9ucyI6ICJ1c2VyIiwgInZlcnNpb24iOiAxfQAABiArxoxZLtggKwRa42gQYLQ4va0sXc9VlcXQJRL1JF1m1Q --repository testpypi dist/*
